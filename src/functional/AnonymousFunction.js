ddac01392f112e3c44e9d5f30c3a364bbff790ff
Victor <em3888@gmail.com>
2017-09-06 21:58:06 +0800

    Finally figure out what is anonymous function(lambda)

--- src/functional/AnonymousFunction.js
+++ src/functional/AnonymousFunction.js
@@ -7,5 +7,8 @@ const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //     return n % 2 === 0;
 // });
 
-const divisibleByTwo= number.filter(number => number%2 ===0)
+const divisibleByTwo= number.filter((number) => number%2 ===0)  ///This is it for lambda express
+//()=> single variable do not need
+//And this is anonymous function because it do not have a name
+
 console.log(divisibleByTwo)
