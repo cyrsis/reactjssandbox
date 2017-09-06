2b7584f3fad62965aad999c4e19ad348a7cb9b5d
Victor <em3888@gmail.com>
2017-09-06 22:55:50 +0800

    Add IIFE to the code

---
+++ src/es6/functionInvokeItself.js
@@ -0,0 +1,11 @@
+(function () {
+    var name = 'victor'
+})();
+
+//This can be run when the brower load the script
+//Immediately-Invoked function Expression (IIFE)
+
+console.log(`The name can be access in here? ${name}`) //name is not define,
+
+// The problem is that the brower have a 'name' variable already, we need a way to use 'name; variable
+
--- src/es6/objectCFreeze.js
+++ src/es6/objectCFreeze.js
@@ -6,4 +6,15 @@ const person = {
 person.name ='Ann'
 console.log(`Person.name can be change? ${person.name}`)
 
-//The atribute of the person can be change 
+//The atribute of the person can be change
+
+const realperson = {
+    name:'victor',
+    age:35
+}
+victor = Object.freeze(realperson);
+
+victor.name= 'Tony'
+
+console.log(`Person.name can be change wth Object.Freeze? ${victor.name}`)
+//victor, the object wont change
