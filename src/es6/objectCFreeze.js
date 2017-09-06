622bf589a55bd55beb6a3192e283065220c90215
Victor <em3888@gmail.com>
2017-09-06 22:47:38 +0800

    Working on the var let Const

---
+++ src/es6/objectCFreeze.js
@@ -0,0 +1,9 @@
+const person = {
+    name:'victor',
+    age:35
+}
+
+person.name ='Ann'
+console.log(`Person.name can be change? ${person.name}`)
+
+//The atribute of the person can be change 
---
+++ src/es6/varLetConst.js
@@ -0,0 +1,15 @@
+//Just use const
+
+//const is block scope
+//let is block scope
+var age = 20;
+
+if (age >18) {
+    const dogyear = age*7
+    console.log(`You dog have ${dogyear}`);
+    
+}
+//var is global
+
+
+
