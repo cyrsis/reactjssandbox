3d7aeb6562b8c305df5383ee97cb727fca47e62b
Victor <em3888@gmail.com>
2017-09-11 10:30:02 +0800

    the reducer action folder create by myslef

--- src/index.js
+++ src/index.js
@@ -8,14 +8,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
+import {createStore} from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
+
+const store = createStore();
 //To test out some javascript from example dir
 //import {constAndLet} from "./examples/constAndLet";
 
 //constAndLet(); //use to test out function, this is great for testing
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
     <App />
     </Provider>
     , document.getElementById('root'));
