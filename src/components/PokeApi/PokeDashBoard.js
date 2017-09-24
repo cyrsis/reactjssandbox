ab951355638039735c5e22a2bb103085b7d99ac6
Victor <em3888@gmail.com>
2017-09-24 23:26:25 +0800

    Start working on the Poke APi

---
+++ src/components/PokeApi/PokeDashBoard.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css'
+
+class PokeDashBoard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            item  : []
+    }
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PokeDashBoard;
